28bc5b45313960437fdc1532a81e886c4c4890c0
Danny Zhu <dzhu@hpe.com>
2022-04-19 01:11:35 -0600

    feat: allow OIDC to authenticate users using arbitrary claims
    Previously, we hard-coded authentication by matching the `email` field
    in the OIDC claim to the username provided through SCIM; now we allow
    matching any claim from the OIDC token to any attribute from SCIM to
    perform authentication.

--- master/internal/config/config.go
+++ master/internal/config/config.go
@@ -119,6 +119,10 @@ func DefaultConfig() *Config {
 		},
 		FeatureSwitches: []string{},
 		ResourceConfig:  *DefaultResourceConfig(),
+		OIDC: OIDCConfig{
+			AuthenticationClaim:         "email",
+			SCIMAuthenticationAttribute: "userName",
+		},
 	}
 }
 

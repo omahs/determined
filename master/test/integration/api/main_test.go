2f3aec159e2d33b2b67800b9f439999ca7289829
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-07-20 13:43:43 -0700

    check for integration flag before resolving elastic

--- master/test/integration/api/main_test.go
+++ master/test/integration/api/main_test.go
@@ -4,6 +4,7 @@
 package api
 
 import (
+	"flag"
 	"log"
 	"os"
 	"testing"
@@ -14,22 +15,27 @@ import (
 	"github.com/determined-ai/determined/master/test/testutils"
 )
 
+var integration = flag.Bool("integration", false, "run integration tests")
+
 var (
 	pgDB *db.PgDB
 	es   *elastic.Elastic
 )
 
 func TestMain(m *testing.M) {
+	flag.Parse()
 	var err error
 	pgDB, err = db.ResolveTestPostgres()
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-	es, err = testutils.ResolveElastic()
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+	if *integration {
+		es, err = testutils.ResolveElastic()
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
 	}
 	os.Exit(m.Run())
 }

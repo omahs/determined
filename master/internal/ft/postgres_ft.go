f89ec113b7eb3bb6eb166fbaec745809487f3c46
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-09-12 00:00:00 -0700

    scaffold

--- master/internal/ft/postgres_ft.go
+++ master/internal/ft/postgres_ft.go
@@ -1,6 +1,18 @@
 package ft
 
-// SaveAlert persists an alert to the database.
-func SaveAlert() {
+import (
+	"github.com/determined-ai/determined/proto/pkg/agentv1"
+)
+
+// we could probably implement just in memory for phase one.
+
+// AddAlert persists an alert to the database.
+func AddAlert(alert *agentv1.RunAlert) (err error) {
+	return
+}
+
+// GetAlerts retrieves all alerts for a given job.
+func GetAlerts(jobID string) (alerts []*agentv1.RunAlert, err error) {
+	// has allocation id and task id, rp, node id
 	return
 }
---
+++ master/pkg/model/task_alert.go
@@ -0,0 +1,4 @@
+package model
+
+// TaskAlert is the db representation of a task alert.
+type TaskAlert struct{}

fe8851874fe52e984a1a78c4e57fdeaafe255903
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-10-30 14:42:23 -0400

    prep non code

--- master/get-deps.sh
+++ master/get-deps.sh
@@ -5,7 +5,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.14.6
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v1.14.6
 
-# Master deps.
+# Cluster deps.
 go install mvdan.cc/gofumpt@v0.4.0
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.1
 go install github.com/bufbuild/buf/cmd/buf@v0.42.1

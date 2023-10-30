c34b6992fbf1bac990c19567d4bdde897640368f
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-10-30 15:13:37 -0400

    big rename

--- cluster/test/testutils/fixtures.go
+++ cluster/test/testutils/fixtures.go
@@ -196,3 +196,48 @@ func APICredentials(ctx context.Context, cl apiv1.DeterminedClient) (context.Con
 	return metadata.AppendToOutgoingContext(
 		ctx, "x-user-token", fmt.Sprintf("Bearer %s", resp.Token)), nil
 }
+
+const (
+	defaultMasterPort = 5152
+)
+
+// DefaultAgentConfig returns a default agent config, for tests.
+func DefaultAgentConfig(offset int) options.Options {
+	// Same defaults as set by viper when binding environment variables.
+	return options.Options{
+		AgentID:             "test-agent" + strconv.Itoa(offset),
+		MasterHost:          "localhost",
+		MasterPort:          defaultMasterPort + offset,
+		ContainerMasterHost: DefaultContainerMasterHost(),
+		ContainerMasterPort: defaultMasterPort + offset,
+		SlotType:            "auto",
+		BindIP:              "0.0.0.0",
+		BindPort:            9090 + offset,
+	}
+}
+
+// DefaultMasterSetAgentConfig returns default MasterSetAgentOptions (logs go to master).
+func DefaultMasterSetAgentConfig() aproto.MasterSetAgentOptions {
+	return aproto.MasterSetAgentOptions{
+		MasterInfo: aproto.MasterInfo{},
+		LoggingOptions: model.LoggingConfig{
+			DefaultLoggingConfig: &model.DefaultLoggingConfig{},
+		},
+	}
+}
+
+// ElasticMasterSetAgentConfig returns MasterSetAgentOptions for an elastic-configured cluster.
+func ElasticMasterSetAgentConfig() aproto.MasterSetAgentOptions {
+	return aproto.MasterSetAgentOptions{
+		MasterInfo:     aproto.MasterInfo{},
+		LoggingOptions: testutils.DefaultElasticConfig(),
+	}
+}
+
+// DefaultContainerMasterHost returns the default container master host, depending on the system.
+func DefaultContainerMasterHost() string {
+	if runtime.GOOS == "darwin" {
+		return "host.docker.internal"
+	}
+	return ""
+}
